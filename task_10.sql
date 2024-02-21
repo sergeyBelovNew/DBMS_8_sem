b8d207a00e975fb130ddce3a57b26b841ed5ead2
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2024-02-21 14:00:58 +0300

    Update task_10.sql

--- task_10.sql
+++ task_10.sql
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 --1. Найти максимальную цену товара (ListPrice) из таблицы Production.Product.
 SELECT MAX("ListPrice") AS MaxPrice
 FROM "Production"."Product";
 
+=======
+--10. Найти максимальную цену товара (ListPrice) из таблицы Production.Product.
+SELECT MAX("ListPrice") AS MaxPrice
+FROM "Production"."Product";
+>>>>>>> Stashed changes
