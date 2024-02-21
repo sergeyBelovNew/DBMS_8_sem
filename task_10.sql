69491c6e396298692e7d8b926ad8e8d1ab5f6b53
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2024-02-21 14:02:49 +0300

    Update task_10.sql
    Fix number of task in comment (1 to 10)

--- task_10.sql
+++ task_10.sql
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
---1. Найти максимальную цену товара (ListPrice) из таблицы Production.Product.
-SELECT MAX("ListPrice") AS MaxPrice
-FROM "Production"."Product";
-
-=======
 --10. Найти максимальную цену товара (ListPrice) из таблицы Production.Product.
 SELECT MAX("ListPrice") AS MaxPrice
 FROM "Production"."Product";
->>>>>>> Stashed changes
