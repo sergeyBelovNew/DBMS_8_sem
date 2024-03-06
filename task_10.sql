ec3ad8d45273a79fa92f2fdbbc9e4b31736f32eb
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2024-03-06 12:25:35 +0300

    Dont use UPDATE or CASE in 10 script

--- task_10.sql
+++ task_10.sql
@@ -1,9 +1,7 @@
 --10. Заменить на NULL значение в поле цвет (Color), если цвет товара красный (red). 
 --Показать поля ProductID, Name, Color из таблицы Production.Product, если цвет товара определен (поле не пустое).
 
-UPDATE "Production"."Product"
-SET "Color" = NULLIF("Color", 'red');
-
-SELECT "ProductID", "Name", "Color"
+SELECT "ProductID", "Name", NULLIF("Color", 'red') AS Color
 FROM "Production"."Product"
 WHERE "Color" IS NOT NULL;
+
