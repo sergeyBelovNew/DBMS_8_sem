b078d87495a2b15031903137f98f35f4a69d2a1d
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2024-03-10 17:24:48 +0300

    UPD

--- task_5.sql
+++ task_5.sql
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
---5. Вывести BusinessEntityID, LastReceiptDate и среднее по StandardPrice для BusinessEntityID с разными LastReceiptDate, 
---предусмотреть вывод общего среднего для всех StandardPrice у различных BusinessEntityID из таблицы Purchasing.ProductVendor. 
---(Использовать ROOLUP)
-
-SELECT "BusinessEntityID", "LastReceiptDate", AVG("StandardPrice") AS AveragePrice
-FROM "Purchasing"."ProductVendor"
-GROUP BY rollup ("BusinessEntityID", "LastReceiptDate");
-=======
 --5. Вывести цвет Color, ProductLine и максимальный размер Size
 -- для каждого цвета Color с разными линиями ProductLine,
 -- предусмотреть вывод общего максимального размера для всех
@@ -21,4 +12,3 @@ SELECT
     MAX("Size") AS MAX_SIZE
 FROM "Production"."Product"
 GROUP BY ROLLUP ("ProductLine", "Color", "Size");
->>>>>>> Stashed changes
