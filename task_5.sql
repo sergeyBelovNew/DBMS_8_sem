80cc5309c6529e2e48dfc89c3b6f4cdbb9fce4fb
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2024-03-12 12:08:48 +0300

    Add GROUPING

---
+++ task_5.sql
@@ -0,0 +1,15 @@
+--5. Вывести цвет Color, ProductLine и максимальный размер Size
+-- для каждого цвета Color с разными линиями ProductLine,
+-- предусмотреть вывод общего максимального размера для всех
+-- цветов Color у различных ProductLine из таблицы
+-- Production.Product. (Использовать ROOLUP). Отделить строки,
+-- созданные с помощью агрегатных функций от строк из фактической
+-- таблицы
+
+SELECT
+    DISTINCT "ProductLine",
+    "Color",
+    MAX("Size") AS MAX_SIZE,
+    GROUPING ("Size")
+FROM "Production"."Product"
+GROUP BY ROLLUP ("ProductLine", "Color", "Size");
